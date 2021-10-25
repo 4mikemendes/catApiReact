3e5edf57611f577901a19fbf2ba8a55a535b33b6
mike mendes <4mikemendes@gmail.com>
2021-10-25 06:53:48 +0200

    catfactApi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      fact: {}
+    }
+    this.eventHandeler = this.eventHandeler.bind(this)
+  }
+  eventHandeler() {
+    fetch("https://catfact.ninja/fact")
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          fact: data
+        })
+      })
+  }
+
+  render() {
+    const text = this.state.fact.fact
+    return (
+      <div>
+        <div className="App">
+          <h1>Cat facts!</h1>
+        <button onClick={this.eventHandeler}>Click Here</button>
+        <p>{text}</p>
+        </div>
+       
+      </div>
+   
+    );
+  }
 }
 
 export default App;
